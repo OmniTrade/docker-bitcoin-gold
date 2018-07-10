a7017bdb382478090bf12f57c512caeaa8f547c2
Fabio Akita <fabioakita@gmail.com>
2018-07-10 11:27:34 -0300

    removing the automatica creation of the bgold.conf as it should be unnecessary

--- 0.15/docker-entrypoint.sh
+++ 0.15/docker-entrypoint.sh
@@ -12,18 +12,6 @@ if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bgoldd" ]; then
   chmod 700 "$BITCOIN_GOLD_DATA"
   chown -R bitcoingold "$BITCOIN_GOLD_DATA"
 
-	if [[ ! -s "$BITCOIN_GOLD_DATA/bgold.conf" ]]; then
-		cat <<-EOF > "$BITCOIN_GOLD_DATA/bgold.conf"
-		server=1
-		printtoconsole=1
-		rpcallowip=::/0
-		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
-		rpcuser=${BITCOIN_RPC_USER:-bitcoingold}
-		walletnotify=/usr/bin/rabbitmqadmin -H ${RABBITMQ_HOST:-localhost} -P 443 --ssl --vhost ${RABBITMQ_USER:-user} -u ${RABBITMQ_USER:-user} -p ${RABBITMQ_PASSWORD:-password} publish routing_key=minerx.deposit.coin payload='{"txid":"%s", "channel_key":"satoshi-gold"}' 
-		EOF
-		chown bitcoingold:bitcoingold "$BITCOIN_GOLD_DATA/bgold.conf"
-	fi
-
   echo "$0: setting data directory to $BITCOIN_GOLD_DATA"
 
   set -- "$@" -datadir="$BITCOIN_GOLD_DATA"
